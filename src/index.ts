0ace61bb8e698b0c8644b600d6dd473db9682507
petri-nykanen <petri.nykaenen@gmail.com>
2025-10-06 21:59:18 +0300

    Typings

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 // src/index.ts
-import express from "express";
+import express, { Request, Response } from "express";
 import { fetchCsvAsJson } from "./fetchCsv";
 
 const app = express();
 const PORT = 3000;
 
-let cachedData: any[] = [];
+let cachedData: unknown[] = [];
 
 const CSV_URL = "https://fimea.fi/documents/147152901/159465988/atc.txt/4f929a39-d6e6-4e85-835f-762993c5a87f?t=1758616780855" // Replace with your actual URL
 
-app.get("/refresh", async (req, res) => {
+app.get("/refresh", async (req : Request, res : Response) => {
   try {
     cachedData = await fetchCsvAsJson(CSV_URL);
     res.json({ message: "Data refreshed", count: cachedData.length });
